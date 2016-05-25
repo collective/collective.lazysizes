b201f5dfc0b2a8c278dfabe5f5fab93153c6cc0e
hvelarde <hector.velarde@gmail.com>
2016-05-25 09:49:55 -0300

    Skip testing coverage on code that  makes little sense to test

--- src/collective/lazysizes/upgrades/__init__.py
+++ src/collective/lazysizes/upgrades/__init__.py
@@ -3,7 +3,7 @@ from collective.lazysizes.logger import logger
 from plone import api
 
 
-def cook_javascript_resources(context):
+def cook_javascript_resources(context):  # pragma: no cover
     """Cook JavaScripts resources."""
     js_tool = api.portal.get_tool('portal_javascripts')
     js_tool.cookResources()
