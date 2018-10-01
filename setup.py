8110d260dcdf215040a227abbeb4b9cc2439c607
hvelarde <hector.velarde@gmail.com>
2018-10-01 16:12:20 -0300

    Back to development: 4.1.3
    [skip ci]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '4.1.2'
+version = '4.1.3.dev0'
 description = 'Integration of lazysizes, a lightweight lazy loader, into Plone.'
 long_description = (
     open('README.rst').read() + '\n' +
